e92bb81ba0dca51314a4688f7b862238b10e1ae1
Sam <sam@meeum.com>
2018-11-04 12:23:48 +1000

    favicons and nav js

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,1 +1,27 @@
 cssVars();
+
+// open nav
+
+$('.nav-prompt').click(function() {
+
+    if ($('.nav-inner').attr('aria-expanded') == 'false') {
+
+        $('.nav-inner').attr('aria-expanded', 'true');
+
+    } else {
+
+        $('.nav-inner').attr('aria-expanded', 'false');
+
+    }
+
+
+    if ($('.nav-prompt').text() == 'Open Navigation') {
+
+        $('.nav-prompt').text('Close Navigation');
+        
+    } else {
+
+        $('.nav-prompt').text('Open Navigation');
+
+    }
+})
